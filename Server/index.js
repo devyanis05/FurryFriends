c620b498925e317ea58f1a463bc30f18673f0da0
poonam-243 <poonambawane828@gmail.com>
2026-01-03 22:38:52 +0530

    Done

--- Server/index.js
+++ Server/index.js
@@ -2,15 +2,28 @@ import e from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import feedback from './modules/feedback.js';
 dotenv.config();
 
 const app = e();
+app.use(e.json());
+app.use(cors());
 const PORT = 5000;
 
-app.post("/hi", (req,res)=>{
-    res.send("hello from server");
+mongoose.connect(process.env.Mongodb)
+.then(()=> console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+app.post("/feedback", async (req ,res)=>{
+    try {
+        const data = new Donate(req.body);
+        await Data.save();
+res.json({ message: " Feedback firm submitted successfully"});
+    } catch (err){
+        res.status(5000).json({error: "Error saving form"});
+    }
 });
 
-app.listen(PORT,()=>{
-    console.log(`your server is running on port ${PORT}`)
-});
+app.listen(process.env.PORT,() =>
+console.log("server running on port ${process.env.PORT} ")
+);
