d3ad9012dd617a2a4c8a442ad06a30c668beda3a
stepee sondawale <steffisondawale06@gmail.com>
2026-01-05 12:34:41 +0530

    now herei have completed my whole work.

--- Client/vite-project/src/FamilyAlbum/Form.jsx
+++ Client/vite-project/src/FamilyAlbum/Form.jsx
@@ -1,81 +1,71 @@
 import React from "react";
-import axios from "axios";`
 
 function Form() {
-    const [photo, setPhoto] = React.useState("");
-    const [name, setName] = React.useState("");
-    const [date, setDate] = React.useState("");
-    const apicall=async()=>{
-      try {
-      const connect =await axios.post("http://localhost:5000/api/auth/login",{
-      photo: }
-
-
   return (
-    <div className="w-screen min-h-screen flex justify-center items-center bg-[#f5e4b3]">
+    <>
+    {/* <div className="h-screen flex items-center justify-center bg-[#221d0b]"> */}
       
-      <div className="w-[420px] bg-[#f5c367] border-2 border-[#81655d] rounded-3xl p-6">
+      {/* Form Card */}
+      <div className="w-[420px] bg-[#f8c873]  h-[520px] flex gap-3 flex-col border-2 border-[#8b5a2b] rounded-3xl p-6">
         
         {/* Title */}
-        <h2 className="text-2xl font-semibold text-center text-[#5c3f36] mb-6">
+        <h2 className="text-2xl   font-semibold text-center text-[#8b5a2b] ">
           Add Your Pet 🌸
         </h2>
 
-        {/* Photo Upload */}
-        <div className="mb-4">
-          <label className=" text-[#5c3f36] mb-1 font-medium">
+        {/* Pet Photo */}
+       
+          <div className=" text-[#8b5a2b]">
             Pet Photo
-          </label>
+          </div>
           <input
             type="file"
-            className="w-full text-sm bg-amber-100 border-2 border-[#81655d] rounded-xl p-2"
+            className="w-full bg-[#fff3c4] border-2 border-[#8b5a2b] rounded-xl p-2"
           />
-        </div>
+        
 
         {/* Pet Name */}
-        <div className="mb-4">
-          <label className=" text-[#5c3f36] mb-1 font-medium">
+          <div className=" text-[#8b5a2b] ">
             Pet Name
-          </label>
+          </div>
           <input
             type="text"
             placeholder="Enter pet name"
-            className="w-full bg-amber-100 border-2 border-[#81655d] rounded-xl p-2 outline-none"
+            className="w-full bg-[#fff3c4] border-2 border-[#8b5a2b] rounded-xl p-2 outline-none"
           />
-        </div>
+        {/* </div> */}
 
         {/* Adoption Date */}
-        <div className="mb-4">
-          <label className="block text-[#5c3f36] mb-1 font-medium">
+       
+          <div className="block text-[#8b5a2b] ">
             Adoption Date
-          </label>
+          </div>
           <input
             type="date"
-            className="w-full bg-amber-100 border-2 border-[#81655d] rounded-xl p-2 outline-none"
+            className="w-full bg-[#fff3c4] border-2 border-[#8b5a2b] rounded-xl p-2 outline-none"
           />
-        </div>
+       
 
         {/* Description */}
-        <div className="mb-6">
-          <label className="block text-[#5c3f36] mb-1 font-medium">
+          <div className="block text-[#8b5a2b] ">
             Description
-          </label>
+          </div>
           <textarea
             rows="3"
-            placeholder="Write something about your pet..."
-            className="w-full bg-amber-100 border-2 border-[#81655d] rounded-xl p-2 outline-none resize-none"
+            placeholder="Describe your pet..."
+            className="w-full bg-[#fff3c4] border-2 border-[#8b5a2b] rounded-xl p-2 outline-none resize-none"
           ></textarea>
-        </div>
+       
 
         {/* Submit Button */}
-        <button className="w-full bg-[#81655d] text-white py-2 rounded-xl text-lg font-semibold hover:bg-[#6f554e] transition">
-          Add to our Family Album
+        <button className="w-full bg-[#ffcc80] border-2 border-[#8b5a2b] rounded-xl  text-[#8b5a2b] font-semibold hover:bg-[#ffc266] h-[80px]"> 
+          Submit
         </button>
 
       </div>
-    </div>
+    {/* </div> */}
+    </>
   );
 }
 
 export default Form;
-
--- Client/vite-project/src/FamilyAlbum/LCont.jsx
+++ Client/vite-project/src/FamilyAlbum/LCont.jsx
@@ -4,19 +4,17 @@ function LCont() {
   return (
     <>
       {/* this section is for the share your story button */}
-      <div className='mr-61.5'>
-
      
-      <div className='h-[30vh] w-[45vw] gap-2 flex flex-col items-center border-3 border-[#5c3f36] rounded-2xl justify-center text-center p-10  bg-[#fac074] ml-[25%]'>
+      <div className='h-[30vh] w-[45vw] gap-2 flex flex-col items-center border-2 border-[#8b5a2b] rounded-2xl justify-center text-center p-10  bg-[#f8c873] '>
         <p className='text-[5vh] text-[#5c3f36]'>Have a Story to Tell</p>   
         <p className='text-[3vh]'>We love to hear how your furry friend is doing! Share your update and join the gallery of happy tails.</p>
 
-        <button 
-        className='h-[9vh] text-[3vh] w-[15vw] bg-[#ffcc80] border-3 border-[#5c3f36] rounded-2xl'>
-          Share Your Story
-          </button>
+        <div
+        className='h-[9vh] text-[3vh] w-auto bg-[#ffcc80]  text-amber-800'>
+          Share Your Story By Filling the form -->
+        </div>
       </div>
-       </div> 
+       
     </>
   )
 }
